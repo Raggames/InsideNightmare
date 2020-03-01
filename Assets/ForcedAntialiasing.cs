0cc66419d10045ad01c68eb193ccb7fa88ce568c
Nicolas Reinhard <nicolasreinhard.pro@gmail.com>
2020-03-01 01:09:15 +0100

    [Big Project Update]
    Added - Sponza 3D Model
    Tested - Sponza in Rendering Scene
    Updated - Sponza materials
    Updated -  VFX Graph
    Changed - Quality Settings
    Tested - High Quality Lightmapping
    Tested - Post Processing Settings

---
+++ Assets/ForcedAntialiasing.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ForcedAntialiasing : MonoBehaviour
+{
+    public Camera cam;
+    public Volume vol;
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.GetUniversalAdditionalCameraData().antialiasing = AntialiasingMode.SubpixelMorphologicalAntiAliasing;
+        cam.GetUniversalAdditionalCameraData().antialiasingQuality = AntialiasingQuality.High;
+
+        vol = FindObjectOfType<Volume>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            cam.GetUniversalAdditionalCameraData().antialiasing = AntialiasingMode.None;
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            cam.GetUniversalAdditionalCameraData().antialiasing = AntialiasingMode.FastApproximateAntialiasing;
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            cam.GetUniversalAdditionalCameraData().antialiasing = AntialiasingMode.SubpixelMorphologicalAntiAliasing;
+            return;
+        }
+
+        if (!Input.GetKeyDown(KeyCode.Keypad4)) return;
+        vol.weight = vol.weight <= 0.01 ? 0 : 1;
+
+    }
+}
